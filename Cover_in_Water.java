3bf433b54ff235c30c22efaaabca058c68ea94dd
Chandra Hans <chandrahans@Chandras-MacBook-Air.local>
2024-03-22 16:42:47 +0530

    edited code

--- Cover_in_Water.java
+++ Cover_in_Water.java
@@ -1,5 +1,33 @@
-public class Cover_in_Water {
-    public static void main(String[]args){
-        System.out.println("hello");
+import java.lang.*;
+import java.io.*;
+
+
+
+public class Cover_in_Water
+{
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String s = br.readLine();
+            int ans = solve(s,n);
+            System.out.println(ans);
+        }
+    }
+    public static int solve(String s,int n){
+        int c=0;
+        int len=s.length();
+        for(int i=0;i<len;i++) {
+            if(s.charAt(i)=='#')
+                continue;
+            else if (i<n-2&&s.charAt(i) == '.' && s.charAt(i + 1) == '.' && s.charAt(i + 2) == '.') {
+                return 2;
+            } else {
+                c++;
+            }
+        }
+        return c;
     }
 }
